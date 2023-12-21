6533a53375c8019dc3189b20d7037c43308da62f
Thi <nguyenthisc1@gmail.com>
2023-12-21 16:03:57 +0700

    update

--- prettier.config.js
+++ prettier.config.js
@@ -17,6 +17,5 @@ module.exports = {
             },
         },
     ],
-    plugins: ['prettier-plugin-tailwindcss','@ianvs/prettier-plugin-sort-imports'],
-    tailwindConfig: './tailwind.config.js',
+    plugins: ['prettier-plugin-tailwindcss'],
 };
